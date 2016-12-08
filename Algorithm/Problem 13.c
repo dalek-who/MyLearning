5bb217a799a25aeb499f2552ad6f189dbe5f9387
245789515 <kj245789515@outlook.com>
2016-12-08 21:01:39 +0800

    up

--- Algorithm/Problem 13.c
+++ Algorithm/Problem 13.c
@@ -119,4 +119,110 @@ int main()
 		printf("YES\n");
 	else printf("NO\n");
 	//printf("%d",MAXFLOW);
+	return 0;
 }
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define L 2500
+int capbility[L][L];
+int backup[L][L] = {0};
+int distance[L]; 
+int a[L], b, c;
+int Point, Head, Tail, MAXFLOW;
+int current[L];
+int min(int x, int y)
+{
+    return (x < y) ? x: y;
+}
+int BFS()
+{
+    int i,j;
+    memset(distance, 0xff, sizeof(distance)); 
+    distance[1] = 0;
+    b = 0;
+    c = 1;
+    a[1] = 1;
+    while(b < c)
+    {
+        j = a[++b];
+        for (i = 1; i <= Point; i++)
+            if (distance[i] < 0 && capbility[j][i] > 0)
+            {
+                distance[i] = distance[j] + 1; 
+                a[++c] = i;
+            }
+    }
+    if(distance[Point] > 0)
+        return 1;
+    else
+        return 0;
+}
+int dinic(int x,int low)
+{
+    int i, flow;
+    if (x == Tail)
+        return low;
+    for(i = current[x]; i <= Point; i++)
+    {
+        current[x] = i;
+        if (capbility[x][i] > 0 && distance[i] == distance[x] + 1) 
+        {
+            flow = dinic(i, min(low, capbility[x][i]));
+            if(flow)
+            {
+                capbility[x][i] -= flow;
+                capbility[i][x] += flow;
+                return flow;
+            }
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int row, col, hole, i, j, k, add;
+    scanf("%d%d%d",&row, &col, &hole);
+	if((row*col-hole)%2==1)
+	{
+		printf("NO");
+		return 0;
+	}
+    for(i = 1; i <= row * col; i++)
+    {
+		if((i/col+i%col)%2 ==0)
+		{
+			capbility[1][1 + i] = 1;
+			if(i <= col * (row -1))
+				capbility[1 + i][1 + (i+col)] = 1;
+			if(i % col != 0)
+				capbility[1 + i][1 + (i+1)] = 1;
+			if(i > col)
+				capbility[1 + i][1 + (i-col)] = 1;
+			if(i % col != 1)
+				capbility[1 + i][1 + (i-1)] = 1;
+		}
+		else capbility[1 + i][1 + row*col + 1] = 1;
+    }
+    for(i = 1; i <= hole; i++)
+    {
+        scanf("%d%d", &j, &k);
+		capbility[1][1 + (j*col-col+k)] = 0;
+		capbility[1 + (j*col-col+k)][1 + row*col + 1]=0;
+    }
+    Head = 1; Tail = 1 + row*col + 1; Point = 1 + row*col + 1;
+    MAXFLOW = 0;
+    while(BFS())
+    {
+        memset(current, 0, sizeof(current));
+        while(add = dinic(Head ,0x7fffffff))
+			MAXFLOW += add; 
+    }//dinic
+	if(MAXFLOW == (row*col-hole)/2)
+		printf("YES\n");
+	else printf("NO\n");
+	//printf("%d",MAXFLOW);
+    return 0;
+}
+*/
