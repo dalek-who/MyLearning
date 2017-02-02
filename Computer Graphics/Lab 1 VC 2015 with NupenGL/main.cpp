67f33e7f68ed9598ff6742e33cc6b40350a82a12
245789515 <kj245789515@outlook.com>
2017-02-02 16:04:38 +0800

    up

--- Computer Graphics/Lab 1 VC 2015 with NupenGL/main.cpp
+++ Computer Graphics/Lab 1 VC 2015 with NupenGL/main.cpp
@@ -4,21 +4,21 @@
 
 /* Include header files depending on platform */
 #ifdef _WIN32
-	#include "GL\freeglut.h"
-	#define M_PI 3.14159
+#include "GL\freeglut.h"
+#define M_PI 3.14159
 #elif __APPLE__
-	#include <OpenGL/gl.h>
-	#include <GLUT/GLUT.h>
+#include <OpenGL/gl.h>
+#include <GLUT/GLUT.h>
 #endif
 
 using namespace std;
 
-float alpha = 0.0, k=1;
-float tx = 0.0, ty=0.0;
+float alpha = 0.0, k = 1;
+float tx = 0.0, ty = 0.0;
 
 void drawcircle(float x, float y, int a, int b, float length)
 {
-	if(a < b)
+	if (a < b)
 		for (int i = a; i <= b; i++)
 		{
 			glVertex2f(x + length * cos(i *  M_PI / 180), y + length * sin(i *  M_PI / 180));
@@ -34,79 +34,100 @@ void profile()
 	glColor3f(0, 0, 0);
 	//body
 	glBegin(GL_LINE_LOOP);
-		drawcircle(-4.9, 7.5, 240, 0, 3.6);
-		drawcircle(0, 0, 100, 67.2, 7.6);
-		glVertex2f(2.95, 7);
-		drawcircle(6.0, 5.3, 150.9, -90, 3.5);
-		glVertex2f(6, 1.8);
-		drawcircle(0, -0.4, 20, -40, 6.4);
-		glVertex2f(6, -6);
-		drawcircle(4.3, -7, 30, -90, 2);
-		drawcircle(-1, -33.5, 78, 102, 25);
-		drawcircle(-6.3, -7, 270, 150, 2);
-		glVertex2f(-7, -3.5);
-		drawcircle(-3.2, -0.4, 215, 150, 5);
+	drawcircle(-4.9, 7.5, 240, 0, 3.6);
+	drawcircle(0, 0, 100, 67.2, 7.6);
+	glVertex2f(2.95, 7);
+	drawcircle(6.0, 5.3, 150.9, -90, 3.5);
+	glVertex2f(6, 1.8);
+	drawcircle(0, -0.4, 20, -40, 6.4);
+	glVertex2f(6, -6);
+	drawcircle(4.3, -7, 30, -90, 2);
+	drawcircle(-1, -33.5, 78, 102, 25);
+	drawcircle(-6.3, -7, 270, 150, 2);
+	glVertex2f(-7, -3.5);
+	drawcircle(-3.2, -0.4, 215, 150, 5);
 	glEnd();
 	//right ear
 	glBegin(GL_LINE_STRIP);
-		drawcircle(7, 3.4, 150, 15, 2.3);
+	glVertex2f(5.2, 4);
+	glVertex2f(5.8, 4.1);
+	glVertex2f(5.1, 4.3);
+	glVertex2f(6.2, 4.6);
+	drawcircle(7, 3.4, 150, 15, 2.3);
 	glEnd();
 	//left ear
 	glBegin(GL_LINE_STRIP);
-		drawcircle(-5.8, 5.94, 0, 240, 1.8);
+	glVertex2f(-4.4, 5.7);
+	glVertex2f(-4.7, 5.9);
+	glVertex2f(-4.4, 6);
+	glVertex2f(-4.5, 6.5);
+	drawcircle(-5.8, 5.94, 0, 240, 1.8);
 	glEnd();
 	//left hand
 	glBegin(GL_LINE_LOOP);
-		drawcircle(-5, -3.8, 0, 360, 0.8);
+	drawcircle(-5, -3.8, 0, 360, 0.8);
 	glEnd();
 	//right hand
 	glBegin(GL_LINE_LOOP);
-		drawcircle(3, -4.5, 0, 360, 0.8);
+	drawcircle(3, -4.5, 0, 360, 0.8);
 	glEnd();
 	//nose
 	glBegin(GL_LINE_STRIP);
-		drawcircle(-1, 0.5, -60, -150, 0.6);
-		drawcircle(-1, 0.55, -150, -60, 0.6);
-		drawcircle(-1, 0.6, -60, -150, 0.6);
+	drawcircle(-1, 0.5, -60, -150, 0.6);
+	drawcircle(-1, 0.55, -150, -60, 0.6);
+	drawcircle(-1, 0.6, -60, -150, 0.6);
 	glEnd();
 	//left foot
 	glBegin(GL_LINE_LOOP);
-		drawcircle(-1, -33.5, 100, 102, 25);
-		drawcircle(-6.3, -7, 270, 220, 2);
-		drawcircle(-7.8, -8.8, 105, 300, 0.8);
+	drawcircle(-1, -33.5, 100, 102, 25);
+	drawcircle(-6.3, -7, 270, 220, 2);
+	drawcircle(-7.8, -8.8, 105, 300, 0.8);
 	glEnd();
 	//right foot
 	glBegin(GL_LINE_LOOP);
-		drawcircle(4.3, -7, -35, -80, 2);
-		drawcircle(5.8, -9, -140, 60, 0.8);
+	drawcircle(4.3, -7, -35, -80, 2);
+	drawcircle(5.8, -9, -140, 60, 0.8);
 	glEnd();
 	//tail
 	glBegin(GL_LINE_LOOP);
-		drawcircle(4.3, -7, 15, -10, 2);
-		drawcircle(6.3, -6.8, -100, 110, 0.6);
+	drawcircle(4.3, -7, 15, -10, 2);
+	drawcircle(6.3, -6.8, -100, 110, 0.6);
+	glEnd();
+	//beard
+	glBegin(GL_LINE_STRIP);
+	drawcircle(-7.5, -3.8, 60, 110, 4);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	drawcircle(-6.5, -5.2, 80, 125, 4);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	drawcircle(4.2, -6.5, 65, 100, 5);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	drawcircle(3.6, -6, 50, 90, 3.5);
 	glEnd();
 	//mouth
 	glBegin(GL_LINE_STRIP);
-		drawcircle(-7.5, -3.8, 60, 110, 4);
+	drawcircle(-2, -1.5, 120, 270, 0.8);
+	drawcircle(-1.4, -2.3, 180, 0, 0.2);
+	drawcircle(-1, 0.2, 270, 300, 2.5);
 	glEnd();
 	glBegin(GL_LINE_STRIP);
-		drawcircle(-6.5, -5.2, 80, 125, 4);
+	glVertex2f(-1.6, -2.3);
+	glVertex2f(-1.4, -3.2);
+	glVertex2f(-1.2, -2.3);
 	glEnd();
-	glBegin(GL_LINE_STRIP);
-		drawcircle(4.2, -6.5, 65, 100, 5);
-	glEnd();
-	glBegin(GL_LINE_STRIP);
-		drawcircle(3.6, -6, 50, 90, 3.5);
-	glEnd();
-	glBegin(GL_LINE_STRIP);
-		drawcircle(-2, -1.5, 120, 270, 0.8);
-		drawcircle(-1.4, -2.3, 180, 0, 0.2);
-		drawcircle(-1, 0.2, 270, 300, 2.5);
-	glEnd();
-	glBegin(GL_LINE_STRIP);
-		glVertex2f(-1.6, -2.3);
-		glVertex2f(-1.4, -3.2);
-		glVertex2f(-1.2, -2.3);
+}
+void shadow()
+{
+	//body
+	glBegin(GL_POLYGON);
+	glColor3f(0.7, 0.7, 0.7);
+	for (int i = 0; i < 360; i++)
+	{
+		if (i < 200 || i > 335)
+			glVertex2f(-1 + 7.5 * cos(i *  M_PI / 180), -9 + 2 * sin(i *  M_PI / 180));
+	}
 	glEnd();
 }
 void yellow()
@@ -114,55 +135,79 @@ void yellow()
 	glColor3f(1, 1, 0.85);
 	//body
 	glBegin(GL_POLYGON);
-		drawcircle(-4.9, 7.5, 240, 0, 3.6);
-		drawcircle(-4.9, 7.5, 240, 0, 3.6);
-		drawcircle(0, 0, 100, 67.2, 7.6);
-		glVertex2f(2.95, 7);
-		glVertex2f(6, 1.8);
-		drawcircle(0, -0.4, 20, -40, 6.4);
-		glVertex2f(6, -6);
-		glVertex2f(-7, -3.5);
-		drawcircle(-3.2, -0.4, 215, 150, 5);
+	drawcircle(-4.9, 7.5, 240, 0, 3.6);
+	drawcircle(-4.9, 7.5, 240, 0, 3.6);
+	drawcircle(0, 0, 100, 67.2, 7.6);
+	glVertex2f(2.95, 7);
+	glVertex2f(6, 1.8);
+	drawcircle(0, -0.4, 20, -40, 6.4);
+	glVertex2f(6, -6);
+	glVertex2f(-7, -3.5);
+	drawcircle(-3.2, -0.4, 215, 150, 5);
 	glEnd();
 	glBegin(GL_POLYGON);
 	glVertex2f(6, -6);
-		drawcircle(4.3, -7, 30, -90, 2);
-		drawcircle(-1, -33.5, 78, 102, 25);
-		drawcircle(-6.3, -7, 270, 150, 2);
-		glVertex2f(-7, -3.5);
+	drawcircle(4.3, -7, 30, -90, 2);
+	drawcircle(-1, -33.5, 78, 102, 25);
+	drawcircle(-6.3, -7, 270, 150, 2);
+	glVertex2f(-7, -3.5);
 	glEnd();
 }
 void orange()
 {
-	glColor3f(1, 0.5, 0);
+	glColor3f(1, 0.4, 0);
 	//head & left ear
 	glBegin(GL_POLYGON);
-		glVertex2f(-2.5, 6.8);
-		drawcircle(-4.9, 7.5, 240, 0, 3.6);
-		drawcircle(0, 0, 100, 67.2, 7.6);
-		glVertex2f(2.95, 7);
-		glVertex2f(6, 1.8);
-		drawcircle(0, -0.4, 20, 0, 6.4);
-		drawcircle(7.8, 11, 250, 180, 11);
+	glVertex2f(-2.5, 6.8);
+	drawcircle(-4.9, 7.5, 240, 0, 3.6);
+	drawcircle(0, 0, 100, 67.2, 7.6);
+	glVertex2f(2.95, 7);
+	glVertex2f(6, 1.8);
+	drawcircle(0, -0.4, 20, 0, 6.4);
+	drawcircle(7.8, 11, 250, 180, 11);
 	glEnd();
 	//right ear
 	glBegin(GL_POLYGON);
-		glVertex2f(2.95, 7);
-		drawcircle(6.0, 5.3, 150.9, -90, 3.5);
-		glVertex2f(6, 1.8);
+	glVertex2f(2.95, 7);
+	drawcircle(6.0, 5.3, 150.9, -90, 3.5);
+	glVertex2f(6, 1.8);
 	glEnd();
 	//right body
 	glBegin(GL_POLYGON);
-		glVertex2f(4.7, -4.7);
-		glVertex2f(4.9, -4.5);
-		glVertex2f(6, -6);
-		drawcircle(4.3, -7, 30, 20, 2);
+	glVertex2f(4.7, -4.7);
+	glVertex2f(4.9, -4.5);
+	glVertex2f(6, -6);
+	drawcircle(4.3, -7, 30, 20, 2);
 	glEnd();
 	//left body
 	glBegin(GL_POLYGON);
-		drawcircle(-6.3, -7, 160, 150, 2);
-		glVertex2f(-7, -3.5);
-		glVertex2f(-6.8, -3.8);
+	drawcircle(-6.3, -7, 160, 150, 2);
+	glVertex2f(-7, -3.5);
+	glVertex2f(-6.8, -3.8);
+	glEnd();
+}
+void ears()
+{
+	//right ear orange
+	glColor3f(1, 0.4, 0);
+	glBegin(GL_POLYGON);
+	glVertex2f(5.2, 4);
+	glVertex2f(5.8, 4.1);
+	glVertex2f(5.1, 4.3);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex2f(5.1, 4.3);
+	glVertex2f(6.2, 4.6);
+	glVertex2f(5, 4.55);
+	glEnd();
+	//left ear yellow
+	glColor3f(1, 1, 0.85);
+	glBegin(GL_POLYGON);
+	glVertex2f(-4.4, 5.7);
+	glVertex2f(-4.7, 5.9);
+	glVertex2f(-4.4, 6);
+	glVertex2f(-4.5, 6.5);
+	glVertex2f(-4, 5.94);
 	glEnd();
 }
 void pink()
@@ -170,54 +215,54 @@ void pink()
 	glColor3f(1, 0.85, 0.85);
 	//left ear
 	glBegin(GL_POLYGON);
-		glVertex2f(-5.8 + 1.8, 5.94);
-		drawcircle(-5.8, 5.94, 0, 240, 1.8);
+	glVertex2f(-5.8 + 1.8, 5.94);
+	drawcircle(-5.8, 5.94, 0, 240, 1.8);
 	glEnd();
 	//right ear
 	glBegin(GL_POLYGON);
-		drawcircle(7, 3.4, 150, 15, 2.3);
-		drawcircle(6.0, 5.3, -22, -90, 3.5);
-		glVertex2f(6, 1.8);
+	drawcircle(7, 3.4, 150, 15, 2.3);
+	drawcircle(6.0, 5.3, -22, -90, 3.5);
+	glVertex2f(6, 1.8);
 	glEnd();
 	//left hand
 	glBegin(GL_POLYGON);
-		drawcircle(-5, -3.8, 0, 360, 0.8);
+	drawcircle(-5, -3.8, 0, 360, 0.8);
 	glEnd();
 	//right hand
 	glBegin(GL_POLYGON);
-		drawcircle(3, -4.5, 0, 360, 0.8);
+	drawcircle(3, -4.5, 0, 360, 0.8);
 	glEnd();
 	//nose
 	glBegin(GL_POLYGON);
-		glVertex2f(-1, 0.3);
-		drawcircle(-1, 0.6, -60, -150, 0.6);
+	glVertex2f(-1, 0.3);
+	drawcircle(-1, 0.6, -60, -150, 0.6);
 	glEnd();
 	//left foot
 	glBegin(GL_POLYGON);
-		drawcircle(-1, -33.5, 100, 102, 25);
-		drawcircle(-6.3, -7, 270, 220, 2);
-		drawcircle(-7.8, -8.8, 105, 300, 0.8);
+	drawcircle(-1, -33.5, 100, 102, 25);
+	drawcircle(-6.3, -7, 270, 220, 2);
+	drawcircle(-7.8, -8.8, 105, 300, 0.8);
 	glEnd();
 	glBegin(GL_POLYGON);
-		glColor3f(1, 1, 0.85);
-		drawcircle(-1, -33.5, 100, 102, 25);
-		drawcircle(-6.3, -7, 270, 210, 2);
-		glColor3f(1, 0.85, 0.85);
+	glColor3f(1, 1, 0.85);
+	drawcircle(-1, -33.5, 100, 102, 25);
+	drawcircle(-6.3, -7, 270, 210, 2);
+	glColor3f(1, 0.85, 0.85);
 	glEnd();
 	//right foot
 	glBegin(GL_POLYGON);
-		drawcircle(4.3, -7, -35, -80, 2);
-		drawcircle(5.8, -9, -140, 60, 0.8);
+	drawcircle(4.3, -7, -35, -80, 2);
+	drawcircle(5.8, -9, -140, 60, 0.8);
 	glEnd();
 	//tail
 	glBegin(GL_POLYGON);
-		drawcircle(4.3, -7, 15, -10, 2);
-		drawcircle(6.3, -6.8, -100, 110, 0.6);
+	drawcircle(4.3, -7, 15, -10, 2);
+	drawcircle(6.3, -6.8, -100, 110, 0.6);
 	glEnd();
 	//mouth
 	glBegin(GL_POLYGON);
-		drawcircle(-1.4, -2.3, 180, 0, 0.2);
-		glVertex2f(-1.4, -3.2);
+	drawcircle(-1.4, -2.3, 180, 0, 0.2);
+	glVertex2f(-1.4, -3.2);
 	glEnd();
 }
 void eye()
@@ -225,60 +270,77 @@ void eye()
 	//black part
 	glColor3f(0, 0, 0);
 	glBegin(GL_POLYGON);
-		drawcircle(-4, 2, 0, 360, 1.8);
+	drawcircle(-4, 2, 0, 360, 1.8);
 	glEnd();
 	glBegin(GL_POLYGON);
-		drawcircle(3, 1, 0, 360, 1.8);
+	drawcircle(3, 1, 0, 360, 1.8);
 	glEnd();
 	//white part
 	glColor3f(1, 1, 1);
 	glBegin(GL_POLYGON);
-		drawcircle(-3.6, 2.4, 0, 360, 0.9);
+	drawcircle(-3.6, 2.4, 0, 360, 0.9);
 	glEnd();
 	glBegin(GL_POLYGON);
-		drawcircle(2.6, 1.4, 0, 360, 0.9);
+	drawcircle(2.6, 1.4, 0, 360, 0.9);
 	glEnd();
 	glBegin(GL_POLYGON);
-		drawcircle(-4.8, 1.2, 0, 360, 0.5);
+	drawcircle(-4.8, 1.2, 0, 360, 0.5);
 	glEnd();
 	glBegin(GL_POLYGON);
-		drawcircle(3.8, 0.2, 0, 360, 0.5);
+	drawcircle(3.8, 0.2, 0, 360, 0.5);
 	glEnd();
 	glBegin(GL_POLYGON);
-		drawcircle(-5, 2.2, 0, 360, 0.3);
+	drawcircle(-5, 2.2, 0, 360, 0.3);
 	glEnd();
 	glBegin(GL_POLYGON);
-		drawcircle(4, 1.2, 0, 360, 0.3);
+	drawcircle(4, 1.2, 0, 360, 0.3);
 	glEnd();
+}
+GLuint TextFont;
+void showchar(char *str)
+{
+	for (; *str != '\0'; *str++)
+		glCallList(TextFont + *str);
 }
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
 	glPushMatrix();
 
 	//controls transformation
-	glScalef(k, k, k);	
-	glTranslatef(tx, ty, 0);	
+	glScalef(k, k, k);
+	glTranslatef(tx, ty, 0);
 	glRotatef(alpha, 0, 0, 1);
-	
+
 	//draw your stuff here
-	glScalef(0.5f, 0.5f, 0.5f);
+	glScalef(0.5, 0.5, 0.5);
+	shadow();
 	yellow();
 	orange();
 	pink();
+	ears();
 	eye();
 	profile();
 
+	glColor3f(0.5, 0, 1);
+	TextFont = glGenLists(128);
+	wglUseFontBitmaps(wglGetCurrentDC(), 0, 128, TextFont);
+
+	glRasterPos2f(-3, -11);
+	showchar("Hamtaro");
+	glRasterPos2f(-6, -12);
+	showchar("A Cute Golden Hamster");
+
 	glPopMatrix();
-	glFlush ();
+	glFlush();
 }
 
-void reshape (int w, int h)
+void reshape(int w, int h)
 {
-	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 
-	glMatrixMode (GL_PROJECTION);
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
 	glOrtho(-10, 10, -10, 10, -10, 10);
@@ -288,85 +350,85 @@ void reshape (int w, int h)
 
 void init(void)
 {
-	glClearColor (1.0, 1.0, 1.0, 1.0);
-	glShadeModel (GL_SMOOTH);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glShadeModel(GL_SMOOTH);
 }
 
 
 
-void keyboard (unsigned char key, int x, int y)
+void keyboard(unsigned char key, int x, int y)
 {
 	//keys to control scaling - k
 	//keys to control rotation - alpha
 	//keys to control translation - tx, ty
 	switch (key) {
 
-		case 'a':
-			alpha+=10;
-			glutPostRedisplay();
+	case 'a':
+		alpha += 10;
+		glutPostRedisplay();
 		break;
 
-		case 'd':
-			alpha-=10;
-			glutPostRedisplay();
+	case 'd':
+		alpha -= 10;
+		glutPostRedisplay();
 		break;
 
-		case 'q':
-			k+=0.1;
-			glutPostRedisplay();
+	case 'q':
+		k += 0.1;
+		glutPostRedisplay();
 		break;
 
-		case 'e':
-			if(k>0.1)
-				k-=0.1;
-			glutPostRedisplay();
+	case 'e':
+		if (k>0.1)
+			k -= 0.1;
+		glutPostRedisplay();
 		break;
 
-		case 'z':
-			tx-=0.1;
-			glutPostRedisplay();
+	case 'z':
+		tx -= 0.1;
+		glutPostRedisplay();
 		break;
 
-		case 'c':
-			tx+=0.1;
-			glutPostRedisplay();
+	case 'c':
+		tx += 0.1;
+		glutPostRedisplay();
 		break;
 
-		case 's':
-			ty-=0.1;
-			glutPostRedisplay();
+	case 's':
+		ty -= 0.1;
+		glutPostRedisplay();
 		break;
 
-		case 'w':
-			ty+=0.1;
-			glutPostRedisplay();
-		break;
-			
-		case 27:
-			exit(0);
+	case 'w':
+		ty += 0.1;
+		glutPostRedisplay();
 		break;
 
-		default:
+	case 27:
+		exit(0);
+		break;
+
+	default:
 		break;
 	}
 }
 
 int main(int argc, char **argv)
 {
-	cout<<"CS3241 Lab 1\n\n";
-	cout<<"+++++CONTROL BUTTONS+++++++\n\n";
-	cout<<"Scale Up/Down: Q/E\n";
-	cout<<"Rotate Clockwise/Counter-clockwise: A/D\n";
-	cout<<"Move Up/Down: W/S\n";
-	cout<<"Move Left/Right: Z/C\n";
-	cout <<"ESC: Quit\n";
+	cout << "CS3241 Lab 1\n\n";
+	cout << "+++++CONTROL BUTTONS+++++++\n\n";
+	cout << "Scale Up/Down: Q/E\n";
+	cout << "Rotate Clockwise/Counter-clockwise: A/D\n";
+	cout << "Move Up/Down: W/S\n";
+	cout << "Move Left/Right: Z/C\n";
+	cout << "ESC: Quit\n";
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize (600, 600);
-	glutInitWindowPosition (50, 50);
-	glutCreateWindow (argv[0]);
-	init ();
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow(argv[0]);
+	init();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	//glutMouseFunc(mouse);
---
+++ Computer Graphics/Lab 1 VC 2015 with NupenGL/readme.txt
@@ -0,0 +1,23 @@
+Your matric number
+A0164576U
+
+What you are drawing
+Harmtaro-- a cartoon character
+
+Primitives and transformations you have used
+glBegin();//GL_LINE_STRIP¡¢GL_LINE_LOOP¡¢GL_POLYGON
+glEnd();
+glVertex2f();
+glColor3f();
+glScalef();
+glCallList();
+
+Methods you have modified ¨C only display()?
+only display()
+
+Any other things the TA should know?
+No
+
+What is the coolest thing(s) in your drawing
+calculate the angle, radius, and coordinate.
+show letters
